6eae4894c956c42b58ef63d3c0f802c67c30313c
ChrisvanderHorst0308 <112861116+ChrisvanderHorst0308@users.noreply.github.com>
2022-12-12 19:11:13 +0100

    -

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,27 +1,40 @@
 let interaction = document.querySelector('a:nth-of-type(12)')
 let rotation = document.querySelector('a:nth-of-type(1)')
 let beating = document.querySelector('a:nth-of-type(2)')
+let widthAnimation = document.querySelector('a:nth-of-type(3)')
+let swingAnimation = document.querySelector('a:nth-of-type(4)')
+
 
 
 interaction.addEventListener('click', jumpHandler)
 interaction.addEventListener('animationend', jumpHandler)
 
 rotation.addEventListener('mouseover', rotateHandler)
-rotation.addEventListener('animationed', rotateHandler)
+rotation.addEventListener('animationend', rotateHandler)
 
 beating.addEventListener('click' , beatingHandler)
-beating.addEventListener('animationed', beatingHandler)
+beating.addEventListener('animationend', beatingHandler)
+
+widthAnimation.addEventListener('dblclick', widthAnimationHandler)
+console.log(widthAnimation)
+
+swingAnimation.addEventListener()
 
 function jumpHandler() {
   interaction.classList.toggle('jump')
+  
 }
 
 function rotateHandler() {
   rotation.classList.toggle('rotate')
+  
 }
 
 function beatingHandler() {
   beating.classList.toggle('increasing')
 }
  
+function widthAnimationHandler () {
+  widthAnimation.classList.toggle('button3CssAnimatie')
+}
 
