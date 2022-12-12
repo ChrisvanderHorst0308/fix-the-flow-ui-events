dd826f2cfe4a7d8fc6ec3a432a1d24182c554df9
ChrisvanderHorst0308 <112861116+ChrisvanderHorst0308@users.noreply.github.com>
2022-12-12 14:58:21 +0100

    -

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,27 @@
 let interaction = document.querySelector('a:nth-of-type(12)')
+let rotation = document.querySelector('a:nth-of-type(1)')
+let beating = document.querySelector('a:nth-of-type(2)')
+
 
 interaction.addEventListener('click', jumpHandler)
 interaction.addEventListener('animationend', jumpHandler)
 
+rotation.addEventListener('mouseover', rotateHandler)
+rotation.addEventListener('animationed', rotateHandler)
+
+beating.addEventListener('click' , beatingHandler)
+beating.addEventListener('animationed', beatingHandler)
+
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+function rotateHandler() {
+  rotation.classList.toggle('rotate')
+}
+
+function beatingHandler() {
+  beating.classList.toggle('increasing')
+}
+ 
+
